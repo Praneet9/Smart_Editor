1c891f495b91613400e13275f6944e6ee94481e6
gnasherx <pawarg124@gmail.com>
2018-03-18 12:43:35 +0530

    Added some details on first screen

--- Electron/NewEditor/src/small_components/AppLayout.js
+++ Electron/NewEditor/src/small_components/AppLayout.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export default styled.div`
   display: grid;
   height: 100vh;
-  grid-template-columns: 150px 200px 1fr;
-  grid-template-rows: 50px 1fr;
+  grid-template-columns: 220px 1fr 400px;
+  grid-template-rows: 60px 1fr;
 `;
--- Electron/NewEditor/src/small_components/FileSelectText.js
+++ Electron/NewEditor/src/small_components/FileSelectText.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-export default styled.div`
+export default styled.p`
 display: flex;
 padding: 12px;
 margin: 0;
 justify-content: center;
 align-items: center
-flex-direction: column
+flex-direction: row
+font-size: 16px;
+color: #717274;
 `;
