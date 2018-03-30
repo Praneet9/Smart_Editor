8813692476cdb7cf7ba3873bb227b382f10272a0
praneet9 <praneetbomma@gmail.com>
2018-03-30 19:51:59 +0530

    added buffer, solved model load, segregated functions

--- Image_Processing/spell_corrector.py
+++ Image_Processing/spell_corrector.py
@@ -4,27 +4,27 @@ from collections import Counter
 def words(text):
 	return re.findall(r'\w+', text.lower())
 
-WORDS = Counter(words(open('./Helping_Libraries/data/all_vocal_books.txt', encoding='utf-8').read()))
+WORDS = Counter(words(open('./data/all_vocal_books.txt', encoding='utf-8').read()))
 
-def P(word, N=sum(WORDS.values())): 
+def P(word, N=sum(WORDS.values())):
     "Probability of `word`."
     return WORDS[word] / N
 
-def correction(word): 
+def correction(word):
     "Most probable spelling correction for word."
     return max(candidates(word), key=P)
 
-def candidates(word): 
+def candidates(word):
     "Generate possible spelling corrections for word."
     return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
 
-def known(words): 
+def known(words):
     "The subset of `words` that appear in the dictionary of WORDS."
     return set(w for w in words if w in WORDS)
 
 def edits1(word):
     "All edits that are one edit away from `word`."
-    letters    = '0123456789abcdefghijklmnopqrstuvwxyz'
+    letters    = 'abcdefghijklmnopqrstuvwxyz0123456789'
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
     deletes    = [L + R[1:]               for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
@@ -32,6 +32,6 @@ def edits1(word):
     inserts    = [L + c + R               for L, R in splits for c in letters]
     return set(deletes + transposes + replaces + inserts)
 
-def edits2(word): 
+def edits2(word):
     "All edits that are two edits away from `word`."
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
