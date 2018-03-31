a80ea8b952f6897a7a53fde4cdad9ecff2566497
AdivarekarBhumit <bhumit97ad@gmail.com>
2018-03-31 14:08:31 +0530

    Commited

--- Website/Frontend/app.py
+++ Website/Frontend/app.py
@@ -53,9 +53,14 @@ def pdftoimage():
 @app.route('/tesseract', methods=['POST'])
 def getTags():
     initialimagename = request.form.get('imagename')
-    imagename = initialimagename[:-1]
+    #imagename = initialimagename[:-1]
+    imagename = initialimagename
+    print(imagename)
     initialcoordinates = request.form.get('coordinates')
     coordinates = initialcoordinates[:-1]
+    photo = request.form.get('photo')
+    sign = request.form.get('sign')
+    print(photo, sign)
     print(coordinates)
     labeldict = {}
     index = 0
@@ -67,7 +72,21 @@ def getTags():
     label_coordinates = []
     i = 0
     datalist = []
+    picturesdict = {}
     for x, y, w, h in tcoords:
+
+        if index < 2:
+            if (photo == 'True'):
+                picturesdict['photo'] = int(x), int(y), int(w), int(h)
+                index = index + 1
+                print('In')
+                continue
+            if (sign == 'True'):
+                picturesdict['sign'] = int(x), int(y), int(w), int(h)
+                index = index + 1
+                print("In")
+                continue
+        print(index)
         x, y, w, h = int(x), int(y), int(w), int(h)
         croppedSection = myocr.cropImage(x, y, w, h, image)
         label = tesseract.image_to_string(croppedSection).replace('\\','')
@@ -78,21 +97,32 @@ def getTags():
         datalist.append(coorlist)
         index = index + 1
     #print(datalist)
-    return render_template('render.html', imagename = imagename, datalist = datalist)
+    return render_template('render.html', imagename = imagename, datalist = datalist, picturesdict = picturesdict)
 
 @app.route('/something', methods=['POST'])
 def save_labels():
-    temp_dict = {'imagename': request.form.get('imagename')}
+    temp_dict = {}
+    temp_dict['imagename'] = request.form.get('imagename')
+    icount= 0
+    if (request.form.get('photo') != None):
+        icount = icount + 1
+        temp_dict['croppedphoto'] = request.form.get('photo')
+    if (request.form.get('sign') != None):
+        icount = icount + 1
+        temp_dict['croppedsign'] = request.form.get('sign')
     counter = int(request.form.get('counter'))
-    for i in range(counter):
+    photo = request.form.get('photo')
+    sign = request.form.get('sign')
+    for i in range(counter - icount):
         # print(request.form.get(str(i)))
         # print(request.form.get(str(i) + "coordinates"))
         # args_dict = {request.form.get(str(i)): request.form.get(str(i) + 'coordinates')}
-        temp_key = request.form.get(str(i)).replace('.', '')
+        temp_key = request.form.get(str(i + icount)).replace('.', '')
         temp_key = myocr.remove_punctuations(temp_key)
         temp_dict[temp_key] = request.form.get(str(i) + 'coordinates').replace('/', '')
+    print(temp_dict)
     db.insert_data('non_filled', temp_dict)
-    cols = db.read_data('non_filled')
+    #cols = db.read_data('non_filled')
     return render_template('index.html', notification = True)
 
 @app.route('/filled')
@@ -148,11 +178,16 @@ def ocr():
     filledimage = "./static/filled_images/" + time + "/" + "filled_" + str(i) + ".jpg"
     output_img.write(filledimage)
     cols = db.read_data('non_filled')
+    tempdict = {}
     for c in cols:
         if c['imagename'] == nonfilledimagename:
             del c['_id']
             del c['imagename']
             for key, values in c.items():
+                if key == 'photo':
+                    temp_dict['photo'] = values
+                if key == 'sign':
+                    temp_dict['sign'] = values
                 labelncoor[key] = values
     print(labelncoor)
     nonfilledimage = cv2.imread(nonfilledimagename, 0)
@@ -160,12 +195,13 @@ def ocr():
     nonfilledimagedilated = cv2.dilate(nonfilledimage.copy(), np.ones((2, 2), np.uint8), iterations = 4)
     filledimage = cv2.threshold(cv2.imread(filledimage, 0), 100, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     filledimagedilated = cv2.dilate(filledimage, np.ones((2, 2), np.uint8), iterations=4)
-
+    print(tempdict)
+    #temp = filledimage[temp_di
     formvalues = myocr.getNonfilledCroppedSections(nonfilledimage, filledimage, nonfilledimagedilated, filledimagedilated, labelncoor)
     #print(formvalues)
     ## inserting into database
     #insert_data('filled', args_dict=formvalues)
-    return render_template('forms.html', formvalues=formvalues)
+    return render_template('forms.html', formvalues=formvalues, picturesdict = tempdict)
 
 @app.route('/saveformvalues', methods=['POST'])
 def saveformvalues():
